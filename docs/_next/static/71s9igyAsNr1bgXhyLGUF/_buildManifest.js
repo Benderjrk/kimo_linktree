78afd9266a31a7e2e62c1d3c4d2f49cf91b9721b
Cardano G.O.A.T <Jrkbender@gmail.com>
2022-10-14 09:59:08 -0400

    testing docs

---
+++ docs/_next/static/71s9igyAsNr1bgXhyLGUF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/790-8c66af67be7b0d7e.js","static/chunks/pages/index-507ba4aaa0725f18.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
